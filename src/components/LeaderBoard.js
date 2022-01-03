6ad04e62c196c9a5ec8044f2838378e93ef65975
Codify KING <81686044+Ifeanyi-Kingsley-Nwabor@users.noreply.github.com>
2022-01-03 16:06:38 +0100

    commit coming soon leader board

--- src/components/LeaderBoard.js
+++ src/components/LeaderBoard.js
@@ -1,13 +1,12 @@
-import React from 'react'
+import React from "react";
 
 const LeaderBoard = () => {
-    return (
-        <main style={{ padding: "1rem 0" }}>
-            <h2>
-                Leader Board!!!
-            </h2>
-        </main>
-    )
-}
+  return (
+    <div className="leaderboard">
+      <h2>Leader Board!!!</h2>
+      <h4>Coming soon...</h4>
+    </div>
+  );
+};
 
-export default LeaderBoard
+export default LeaderBoard;
